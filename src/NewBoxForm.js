a45f3e89500433e8ebf809fddf4ef6879ab8f309
Brian Aston <astonb.dev@gmail.com>
2022-03-03 12:33:48 -0800

    Finishing implementation of newBoxForm

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -1,25 +1,36 @@
+import { hasUnreliableEmptyValue } from "@testing-library/user-event/dist/utils";
 import {useState} from "react";
 
-function NewBoxForm(){
+function NewBoxForm({addBox}){
     const [formData, setFormData] = useState({
         height:"",
         width:"",
         backgroundColor:""
-    })
+    });
 
-    function handleSubmit(){
+    function handleSubmit(evt){
+        evt.preventDefault();
 
+        setFormData(currData => (
+            {...currData,
+                width: evt.target.getElementById("width").value,
+                height: evt.target.getElementById("height").value,
+                backgroundColor: evt.target.getElementById("background-color").value
+            }));
+
+        addBox(() => formData);
     }
 
     return(
-        <form action="">
+        <form action=""
+              onSubmit={handleSubmit}>
             <label htmlFor="height">Height</label>
             <input type="text" id="height" name="height" />
             <label htmlFor="width">Width</label>
             <input type="text" id="width" name="width" />
             <label htmlFor="backgroundColor">Background Color</label>
             <input type="text" id="background-color" name="backgroundColor" />
-            <button type="submit">Add a new box!</button>
+            <button type="submit" >Add a new box!</button>
         </form>
     )
 }
